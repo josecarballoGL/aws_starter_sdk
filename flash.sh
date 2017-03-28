a688734b25540142823f3ff79f0ff97a4b113cb5
Jose Carballo <jose.carballo@gorillalogic.com>
2017-03-28 13:46:04 -0600

    More components to the flashing script.

--- flash.sh
+++ flash.sh
@@ -5,5 +5,8 @@ FLASHPROG_PATH=sdk/tools/OpenOCD/flashprog.py
 # Erase everything in Flash
 #sudo python sdk/tools/OpenOCD/flashprog.py -l sdk/tools/OpenOCD/mw300/layout.txt
 
-sudo $FLASHPROG_PATH -i ftdi --mcufw bin/mw302_rd/hello_world.bin \
-		--boot2 boot2/boot2.bin
+sudo $FLASHPROG_PATH -i ftdi \
+		--mcufw bin/mw302_rd/aws_starter_demo.bin \
+		--boot2 boot2/boot2.bin \
+		--wififw wifi-firmware/mw30x/mw30x_uapsta_14.76.36.p103.bin.xz \
+		--ftfs bin/mw302_rd/aws_starter_demo.ftfs
